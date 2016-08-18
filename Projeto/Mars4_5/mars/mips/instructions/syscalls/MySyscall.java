7a92b0cb67b9fa2a041144521f598fe36bbec3e5
Rodolfo Medeiros <rodolfofelipe15@gmail.com>
2016-08-18 12:37:38 -0300

    Com os exemplos exibidos em sala...

---
+++ Projeto/Mars4_5/mars/mips/instructions/syscalls/MySyscall.java
@@ -0,0 +1,24 @@
+// Editado Rodolfo
+
+package mars.mips.instructions.syscalls;
+
+import mars.ProcessingException;
+import mars.ProgramStatement;
+import mars.mips.hardware.Register;
+import mars.mips.hardware.RegisterFile;
+import mars.util.SystemIO;
+
+public class MySyscall extends AbstractSyscall{
+	
+	public MySyscall() {
+		super(18, "MySyscall");
+	}
+
+	@Override
+	public void simulate(ProgramStatement statement) throws ProcessingException {
+		SystemIO.printString("Olá Jerfferson, tudo bem? garotinho!!!!");
+		//System.out.println("Minuno bunito!");
+		SystemIO.printString(RegisterFile.getProgramCounter()+"");
+	}
+		
+}
